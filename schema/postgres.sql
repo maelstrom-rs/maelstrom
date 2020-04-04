90d290b0f0a42ea38e03d7bdd8a52d9dca0425b0
Chris Bruce <chris@chrisbruce.com>
2020-04-04 17:11:00 +0200

    adds accounts schema

---
+++ schema/postgres.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS accounts (
+  -- The Matrix user ID localpart for this account
+  localpart TEXT NOT NULL PRIMARY KEY,
+  -- When this account was first created, as a unix timestamp (ms resolution).
+  created_ts BIGINT NOT NULL,
+  -- The password hash for this account. Can be NULL if this is a passwordless account.
+  password_hash TEXT,
+  -- Identifies which application service this account belongs to, if any.
+  appservice_id TEXT,
+  -- Is this account a server admin
+  is_admin bool DEFAULT FALSE NOT NULL,
+  is_guest bool DEFAULT FALSE NOT NULL
+);
+CREATE INDEX IF NOT EXISTS idx_accounts_is_guest ON accounts(is_guest);
