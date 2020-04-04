4c748d2b0199351e4730191aa202ab19ef6048c0
Chris Bruce <chris@chrisbruce.com>
2020-04-04 19:42:47 +0200

    change response name

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let config = api::ServerConfig::new_from_env();
-    let _res = api::start(config).await;
+    let _server = api::start(config).await;
 
     Ok(())
 }
