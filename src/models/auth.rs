1ad5de1e29636d6f4d512f2bd7a1932ba94a53c2
sparky8251 <sparky@possumlodge.me>
2020-04-12 14:39:37 -0400

    Now using ruma_identifiers to provider UserId

--- src/models/auth.rs
+++ src/models/auth.rs
@@ -1,5 +1,7 @@
 use std::borrow::Cow;
 
+use ruma_identifiers::UserId;
+
 use crate::CONFIG;
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
@@ -16,57 +18,6 @@ pub struct LoginFlow {
     pub login_type: LoginType,
 }
 
-#[derive(Clone, Debug)]
-pub struct UserId {
-    pub local_part: String,
-    pub domain: Cow<'static, str>,
-}
-impl<'de> serde::Deserialize<'de> for UserId {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let mut str_id: String = serde::Deserialize::deserialize(deserializer)?;
-        if let Some(colon_idx) = str_id.find(":") {
-            let domain = str_id.split_off(colon_idx + 1);
-            str_id.truncate(str_id.len() - 1);
-            Ok(UserId {
-                local_part: str_id,
-                domain: Cow::Owned(domain),
-            })
-        } else {
-            Ok(UserId {
-                local_part: str_id,
-                domain: Cow::Borrowed(&CONFIG.hostname),
-            })
-        }
-    }
-}
-impl serde::Serialize for UserId {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_str(&format!("{}:{}", self.local_part, self.domain))
-    }
-}
-
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
-#[serde(tag = "medium")]
-#[serde(rename_all = "lowercase")]
-pub enum ThirdParty {
-    Email { address: String },
-    MSISDN { address: String },
-}
-
-#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
-#[serde(tag = "type")]
-pub enum UserIdentifier {
-    #[serde(rename = "m.id.user")]
-    UserId { user: UserId },
-    #[serde(rename = "m.id.thirdparty")]
-    ThirdParty(ThirdParty),
-    #[serde(rename = "m.id.phone")]
-    PhoneNumber {
-        country: String, // Alpha2
-        phone: String,
-    },
-}
-
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
 #[serde(tag = "type")]
 pub enum Challenge {
@@ -80,7 +31,7 @@ pub enum Challenge {
 pub struct LoginRequest {
     #[serde(flatten)]
     pub challenge: Challenge,
-    pub identifier: UserIdentifier,
+    pub identifier: UserId,
     pub device_id: Option<String>,
     pub initial_device_display_name: Option<String>,
 }
